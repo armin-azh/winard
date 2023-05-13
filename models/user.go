7ea56b1eb2814b3c722d2f280fbe8a338ccc54e6
Armin Azhdehnia <armin.azhdehnia@gmail.com>
2023-05-13 09:43:05 -0500

    add user model

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	gorm.Model
+	prime     string `gorm:"unique"`
+	username  string `gorm:"unique"`
+	password  string `gorm:"unique"`
+	lastLogin time.Time
+}
