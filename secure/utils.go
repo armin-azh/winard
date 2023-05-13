575d40a109543387fa9e68271a3ae8fafe520eea
Armin Azhdehnia <armin.azhdehnia@gmail.com>
2023-05-13 15:46:22 -0500

    use pointer

--- secure/utils.go
+++ secure/utils.go
@@ -3,9 +3,8 @@ package secure
 import "golang.org/x/crypto/bcrypt"
 
 // ComparePassword Compare a given password with relative hashed value
-func ComparePassword(payload string, password string) (bool, error) {
-
-	if err := bcrypt.CompareHashAndPassword([]byte(payload), []byte(password)); err != nil {
+func ComparePassword(payload *string, password *string) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword([]byte(*payload), []byte(*password)); err != nil {
 		return false, err
 	}
 	return true, nil
