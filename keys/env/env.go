59c1b1f835e7b3171c8d5507894b9cd308603d9c
Armin Azhdehnia <armin.azhdehnia@gmail.com>
2023-05-15 11:43:47 -0500

    add NATS_URL

--- keys/env/env.go
+++ keys/env/env.go
@@ -1,7 +1,9 @@
 package env
 
 const (
-	EnvDatabaseUrl = "DB_URL"
-	EnvSecret      = "gexpress_secret"
-	EnvNatsUrl     = "NATS_URL"
+	EnvDatabaseUrl   = "DB_URL"
+	EnvSecret        = "gexpress_secret"
+	EnvNatsUrl       = "NATS_URL"
+	EnvNatsClientID  = "NATS_CLIENT_ID"
+	EnvNatsClusterID = "NATS_CLUSTER_ID"
 )
