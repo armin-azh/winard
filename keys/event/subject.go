796b22b66839380c43f2d0f750384a9c26b51e0a
Armin Azhdehnia <armin.azhdehnia@gmail.com>
2023-05-24 16:53:24 -0500

    split subject into created and updated

--- keys/event/subject.go
+++ keys/event/subject.go
@@ -1,9 +1,14 @@
 package event
 
 const (
-	EventSubjectAdmin       = "admin"
-	EventSubjectAgent       = "agent"
-	EventSubjectThirdParty  = "third-party"
-	EventSubjectMessage     = "message"
-	EventSubjectTransaction = "transaction"
+	EventSubjectAdminCreate        = "admin-created"
+	EventSubjectAdminUpdate        = "admin-updated"
+	EventSubjectAgentCreate        = "agent-created"
+	EventSubjectAgentUpdate        = "agent-updated"
+	EventSubjectThirdPartyCreate   = "third-party-created"
+	EventSubjectThirdPartyUpdate   = "third-party-updated"
+	EventSubjectMessageCreated     = "message-created"
+	EventSubjectMessageUpdated     = "message-updated"
+	EventSubjectTransactionCreated = "transaction-created"
+	EventSubjectTransactionUpdated = "transaction-updated"
 )
